7b8c833ba7c3945eb848ce2d7b8b70153b2d139f
Kathryn Williams <ksw4321@gmail.com>
2018-01-28 16:05:31 +0000

    implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
   
+=begin  
   def hello
     render html: "hello world"
   end
+=end
 end
